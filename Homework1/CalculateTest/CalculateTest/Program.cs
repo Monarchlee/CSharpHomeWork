880d75568a3efe438ef82e5dcdfdb92a7990ff7f
Monarchlee <2237064165@qq.com>
2020-02-20 15:35:14 +0800

    Move homework into first package.

---
+++ Homework1/CalculateTest/CalculateTest/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTest
+{
+    class Func
+    {
+
+        public static double Doneit(double x,double y,char opt,ref bool flag)
+        {
+            double result = 0;
+            switch (opt)
+            {
+                case '+':
+                    result = x + y;
+                    flag = true;
+                    break;
+                case '-':
+                    result = x - y;
+                    flag = true;
+                    break;
+                case '*':
+                    result = x * y;
+                    flag = true;
+                    break;
+                case '/':
+                    if (y == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else
+                    {
+                        result = x / y;
+                        flag = true;
+                    }
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "";string num2 = "";
+            double realnum1 = 0, realnum2 = 0, result = 0;
+            string opt="",end="";char realopt = '\0';
+            bool flag = true;
+            Console.WriteLine("This is a calculate.");
+            while(true)
+            {
+                Console.WriteLine("PLS enter the two number needed to be done:");
+                num1 = Console.ReadLine();
+                while (Double.TryParse(num1, out realnum1) == false)
+                {
+                    Console.WriteLine("Invaild input.PLS input a valid num:");
+                    num1 = Console.ReadLine();
+                }
+                num2 = Console.ReadLine();
+                while (Double.TryParse(num2, out realnum2) == false)
+                {
+                    Console.WriteLine("Invaild input.PLS input a valid num:");
+                    num1 = Console.ReadLine();
+                }
+                Console.WriteLine("Now for the option:");
+                opt = Console.ReadLine();
+                while (!Char.TryParse(opt, out realopt))
+                {
+                    Console.WriteLine("Invaild input.PLS input a valid opt:");
+                    opt = Console.ReadLine();
+                }
+                result=Func.Doneit(realnum1, realnum2, realopt, ref flag);
+                if(flag==false)
+                {
+                    Console.WriteLine("Invaild opt for these two nums,do it again.");
+                    continue;
+                }
+                Console.WriteLine($"Your result is {result}");
+                Console.WriteLine("Push z to end,c to continue.");
+                end = Console.ReadLine();
+                while (end!="z"&&end!="c")
+                {
+                    Console.WriteLine("PLS input a valid char");
+                    end = Console.ReadLine();
+                }
+                if (end == "z")
+                    break;
+            }
+        }
+    }
+
+}
---
+++ Homework1/Jisuanqi/Jisuanqi/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Jisuanqi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Let\'s start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(73, 25);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox.Location = new System.Drawing.Point(242, 233);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(66, 23);
+            this.comboBox.TabIndex = 4;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "CLR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Do it!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
