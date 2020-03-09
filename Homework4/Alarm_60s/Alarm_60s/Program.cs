6a1e1e50fffa91c0113d439c7f24bb2b13949548
Monarchlee <2237064165@qq.com>
2020-03-09 16:29:32 +0800

    The fourth homework is on now.

---
+++ Homework4/Alarm_60s/Alarm_60s/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace MyNamespace
+{
+
+    public delegate void AlarmHandler();
+    public class AlarmClock
+    {
+        public event AlarmHandler Alarm;
+        public event AlarmHandler Tick;
+        public AlarmClock()
+        {
+            Tick += delegate { Console.WriteLine("Tick-tock."); };
+        }
+        public int TimeNow { get; set; }
+        public int AlarmTime { get; set; }
+        public void Start()
+        {
+            TimeNow = 0;
+            while (TimeNow < 60)
+            {
+                Thread.Sleep(1000);
+                TimeNow++;
+                Tick();
+                if (TimeNow == AlarmTime)
+                {
+                    Alarm();
+                }
+            }
+        }
+
+
+    }
+    public class User
+    {
+        public AlarmClock myOwnClock = new AlarmClock();
+
+        public User(int alarmInput)
+        {
+            myOwnClock.AlarmTime = alarmInput;
+            myOwnClock.Alarm += delegate { Console.WriteLine("\a It's " + alarmInput + " now!"); };
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int setTime = 0;
+            while (!int.TryParse(Console.ReadLine(), out setTime) || setTime < 0)
+            {
+                Console.WriteLine("PLS input a valid value.");
+            }
+
+            User firstUser = new User(setTime);
+            firstUser.myOwnClock.Start();
+        }
+
+    }
+}
+
---
+++ Homework4/List/List/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ListNameSpace
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> now = head;
+            while (now != null)
+            {
+                action(now.Data);
+                now = now.Next;
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    // 整型List
+                        GenericList<int> intlist = new GenericList<int>();
+                        for (int x = 0; x < 10; x++)
+                        {
+                            intlist.Add(x);
+                        }
+
+                        // 字符串型List
+                        GenericList<string> strList = new GenericList<string>();
+                        for (int x = 0; x < 10; x++)
+                        {
+                            strList.Add("str" + x);
+                        }
+
+                        int sum = 0, min = int.MaxValue, max = int.MinValue;
+                        intlist.ForEach(x =>
+                        {
+                            sum += x;
+                            min = (x < min) ? x : min;
+                            max = (x > max) ? x : max;
+                            Console.Write(x + " ");
+                        });
+                        Console.WriteLine('\n'+"Max of it is " + max);
+                        Console.WriteLine("Min of it is " + min);
+                        Console.WriteLine("Sum of it is " + sum);
+                        strList.ForEach(x => Console.Write(x + " "));
+           
+
+
+        }
+
+    }
+}
